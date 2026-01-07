e3ec10050677a59970ac13e9c0032b664e80dec8
Takuya Okabe <takuyaokabe@gmail.com>
2026-01-07 16:53:42 +0900

    FIX: refactoring

--- screen-weather-get.py
+++ screen-weather-get.py
@@ -4,204 +4,136 @@ import datetime
 import sys
 import os
 import logging
-import locale # 追加
-from weather_providers import climacell, openweathermap, metofficedatahub, metno, meteireann, accuweather, visualcrossing, weathergov, smhi
-from alert_providers import metofficerssfeed, weathergovalerts
-from alert_providers import meteireann as meteireannalertprovider
-from utility import get_formatted_time, update_svg, configure_logging, configure_locale
+import locale
 import textwrap
 import html
-import urllib.parse
+from utility import update_svg, configure_logging, configure_locale
+# 各プロバイダのインポート（そのまま）
+from weather_providers import (climacell, openweathermap, metofficedatahub, 
+                               metno, meteireann, accuweather, visualcrossing, 
+                               weathergov, smhi)
+from alert_providers import metofficerssfeed, weathergovalerts
+from alert_providers import meteireann as meteireannalertprovider
 
 configure_locale()
 configure_logging()
 
 def get_active_locale():
-    """現在の環境の言語コード(ja, en等)を返す"""
     try:
         return locale.getlocale()[0][:2]
     except:
         return "en"
 
 def format_weather_description(weather_description):
-    if len(weather_description) < 20:
+    """天候の説明を2行に分割する"""
+    if len(weather_description) < 15:
         return {1: weather_description, 2: ''}
 
-    splits = textwrap.fill(weather_description, 20, break_long_words=False,
+    splits = textwrap.fill(weather_description, 15, break_long_words=False,
                            max_lines=2, placeholder='...').split('\n')
-    weather_dict = {1: splits[0]}
-    weather_dict[2] = splits[1] if len(splits) > 1 else ''
-    return weather_dict
+    return {1: splits[0], 2: splits[1] if len(splits) > 1 else ''}
 
-def get_weather(location_lat, location_long, units):
+def get_weather_provider(location_lat, location_long, units):
+    """環境変数から適切な気象プロバイダを初期化して返す"""
+    configs = {
+        "visualcrossing": os.getenv("VISUALCROSSING_APIKEY"),
+        "met_eireann": os.getenv("WEATHER_MET_EIREANN"),
+        "weathergov": os.getenv("WEATHERGOV_SELF_IDENTIFICATION"),
+        "metno": os.getenv("METNO_SELF_IDENTIFICATION"),
+        "accuweather": os.getenv("ACCUWEATHER_APIKEY"),
+        "metoffice": os.getenv("METOFFICEDATAHUB_API_KEY"),
+        "openweathermap": os.getenv("OPENWEATHERMAP_APIKEY"),
+        "climacell": os.getenv("CLIMACELL_APIKEY"),
+        "smhi": os.getenv("SMHI_SELF_IDENTIFICATION")
+    }
 
-    # gather relevant environment configs
-    climacell_apikey = os.getenv("CLIMACELL_APIKEY")
-    openweathermap_apikey = os.getenv("OPENWEATHERMAP_APIKEY")
-    metoffice_apikey = os.getenv("METOFFICEDATAHUB_API_KEY")
-    accuweather_apikey = os.getenv("ACCUWEATHER_APIKEY")
-    accuweather_locationkey = os.getenv("ACCUWEATHER_LOCATIONKEY")
-    metno_self_id = os.getenv("METNO_SELF_IDENTIFICATION")
-    visualcrossing_apikey = os.getenv("VISUALCROSSING_APIKEY")
-    use_met_eireann = os.getenv("WEATHER_MET_EIREANN")
-    weathergov_self_id = os.getenv("WEATHERGOV_SELF_IDENTIFICATION")
-    smhi_self_id = os.getenv("SMHI_SELF_IDENTIFICATION")
-
-    if (
-        not climacell_apikey
-        and not openweathermap_apikey
-        and not metoffice_apikey
-        and not accuweather_apikey
-        and not metno_self_id
-        and not visualcrossing_apikey
-        and not use_met_eireann
-        and not weathergov_self_id
-        and not smhi_self_id
-    ):
-        logging.error("No weather provider has been configured (Climacell, OpenWeatherMap, Weather.gov, MetOffice, AccuWeather, Met.no, Met Eireann, VisualCrossing...)")
+    if not any(configs.values()):
+        logging.error("No weather provider configured.")
         sys.exit(1)
 
-    if visualcrossing_apikey:
-        logging.info("Getting weather from Visual Crossing")
-        weather_provider = visualcrossing.VisualCrossing(visualcrossing_apikey, location_lat, location_long, units)
-    elif use_met_eireann:
-        logging.info("Getting weather from Met Eireann")
-        weather_provider = meteireann.MetEireann(location_lat, location_long, units)
-
-    elif weathergov_self_id:
-        logging.info("Getting weather from Weather.gov")
-        weather_provider = weathergov.WeatherGov(weathergov_self_id, location_lat, location_long, units)
-
-    elif metno_self_id:
-        logging.info("Getting weather from Met.no")
-        weather_provider = metno.MetNo(metno_self_id, location_lat, location_long, units)
-
-    elif accuweather_apikey:
-        logging.info("Getting weather from Accuweather")
-        weather_provider = accuweather.AccuWeather(accuweather_apikey, location_lat,
-                                                   location_long,
-                                                   accuweather_locationkey,
-                                                   units)
-    elif metoffice_apikey:
-        logging.info("Getting weather from Met Office Weather Datahub")
-        weather_provider = metofficedatahub.MetOffice(metoffice_apikey,
-                                                      location_lat,
-                                                      location_long,
-                                                      units)
-
-    elif openweathermap_apikey:
-        logging.info("Getting weather from OpenWeatherMap")
-        weather_provider = openweathermap.OpenWeatherMap(openweathermap_apikey,
-                                                         location_lat,
-                                                         location_long,
-                                                         units)
-
-    elif climacell_apikey:
-        logging.info("Getting weather from Climacell")
-        weather_provider = climacell.Climacell(climacell_apikey, location_lat, location_long, units)
-
-    elif smhi_self_id:
-        logging.info("Getting weather from SMHI")
-        weather_provider = smhi.SMHI(smhi_self_id, location_lat, location_long, units)
-
-    weather = weather_provider.get_weather()
-    logging.info("weather - {}".format(weather))
-    return weather
-
-def format_alert_description(alert_message):
-    return html.escape(alert_message)
+    # 優先順位に従ってプロバイダを返却
+    if configs["visualcrossing"]:
+        return visualcrossing.VisualCrossing(configs["visualcrossing"], location_lat, location_long, units)
+    if configs["met_eireann"]:
+        return meteireann.MetEireann(location_lat, location_long, units)
+    if configs["weathergov"]:
+        return weathergov.WeatherGov(configs["weathergov"], location_lat, location_long, units)
+    if configs["metno"]:
+        return metno.MetNo(configs["metno"], location_lat, location_long, units)
+    if configs["accuweather"]:
+        return accuweather.AccuWeather(configs["accuweather"], location_lat, location_long, os.getenv("ACCUWEATHER_LOCATIONKEY"), units)
+    if configs["metoffice"]:
+        return metofficedatahub.MetOffice(configs["metoffice"], location_lat, location_long, units)
+    if configs["openweathermap"]:
+        return openweathermap.OpenWeatherMap(configs["openweathermap"], location_lat, location_long, units)
+    if configs["climacell"]:
+        return climacell.Climacell(configs["climacell"], location_lat, location_long, units)
+    if configs["smhi"]:
+        return smhi.SMHI(configs["smhi"], location_lat, location_long, units)
 
 def get_alert_message(location_lat, location_long):
-    alert_message = ""
-    alert_metoffice_feed_url = os.getenv("ALERT_METOFFICE_FEED_URL")
-    alert_weathergov_self_id = os.getenv("ALERT_WEATHERGOV_SELF_IDENTIFICATION")
-    alert_meteireann_feed_url = os.getenv("ALERT_MET_EIREANN_FEED_URL")
+    alert_metoffice = os.getenv("ALERT_METOFFICE_FEED_URL")
+    alert_weathergov = os.getenv("ALERT_WEATHERGOV_SELF_IDENTIFICATION")
+    alert_meteireann = os.getenv("ALERT_MET_EIREANN_FEED_URL")
 
-    if alert_weathergov_self_id:
-        logging.info("Getting weather alert from Weather.gov API")
-        alert_provider = weathergovalerts.WeatherGovAlerts(location_lat, location_long, alert_weathergov_self_id)
-        alert_message = alert_provider.get_alert()
-
-    elif alert_metoffice_feed_url:
-        logging.info("Getting weather alert from Met Office RSS Feed")
-        alert_provider = metofficerssfeed.MetOfficeRssFeed(alert_metoffice_feed_url)
-        alert_message = alert_provider.get_alert()
-
-    elif alert_meteireann_feed_url:
-        logging.info("Getting weather alert from Met Eireann")
-        alert_provider = meteireannalertprovider.MetEireannAlertProvider(alert_meteireann_feed_url)
-        alert_message = alert_provider.get_alert()
-
-    logging.info("alert - {}".format(alert_message))
-    return alert_message
+    if alert_weathergov:
+        return weathergovalerts.WeatherGovAlerts(location_lat, location_long, alert_weathergov).get_alert()
+    if alert_metoffice:
+        return metofficerssfeed.MetOfficeRssFeed(alert_metoffice).get_alert()
+    if alert_meteireann:
+        return meteireannalertprovider.MetEireannAlertProvider(alert_meteireann).get_alert()
+    return ""
 
 def main():
-    template_name = os.getenv("SCREEN_LAYOUT", "1")
+    template_name = os.getenv("SCREEN_LAYOUT", "6")
     location_lat = os.getenv("WEATHER_LATITUDE", "51.5077")
     location_long = os.getenv("WEATHER_LONGITUDE", "-0.1277")
     weather_format = os.getenv("WEATHER_FORMAT", "CELSIUS")
 
-    if (weather_format == "CELSIUS"):
-        units = "metric"
-        degrees = "°C"
-    else:
-        units = "imperial"
-        degrees = "°F"
+    units, degrees = ("metric", "°C") if weather_format == "CELSIUS" else ("imperial", "°F")
 
-    weather = get_weather(location_lat, location_long, units)
+    provider = get_weather_provider(location_lat, location_long, units)
+    weather = provider.get_weather()
 
     if not weather:
-        logging.error("Unable to fetch weather payload. SVG will not be updated.")
+        logging.error("Unable to fetch weather. SVG will not be updated.")
         return
 
+    # データ整形
     weather_desc = format_weather_description(weather["description"])
-    alert_message = get_alert_message(location_lat, location_long)
-    alert_message = format_alert_description(alert_message)
-
-    today = datetime.datetime.now()
-    time_now = today.strftime("%H:%M") 
-
-    time_now_font_size = "100px"
-
-    if len(time_now) > 6:
-        time_now_font_size = str(100 - (len(time_now)-5) * 5) + "px"
-
-    today = datetime.datetime.now()
+    alert_msg = html.escape(get_alert_message(location_lat, location_long))
+    
+    now = datetime.datetime.now()
     lang = get_active_locale()
 
-    if lang == "ja":
-        day_one_format = "%-m月 %-d日"  # 1月 2日
-        day_name_format = "%A"         # 金曜日
-        hour_now_format = "%H:%M"      # 13:00 (24時間制が一般的)
-    else:
-        day_one_format = "%b %-d"      # Jan 2
-        day_name_format = "%A"         # Friday
-  #      hour_now_format = "%-I %p"     # 1 PM
-        hour_now_format = "%H:%M"     # HH:MM
-    # ----------------------------
+    # ロケール設定
+    date_fmt = "%-m月 %-d日" if lang == "ja" else "%b %-d"
+    
+    # 24時間制 HH:MM に固定
+    time_str = now.strftime("%H:%M")
+    
+    # フォントサイズ調整 (HH:MMは5文字なので通常100pxで固定されます)
+    time_size = "100px"
+    if len(time_str) > 6:
+        time_size = f"{100 - (len(time_str)-5) * 5}px"
 
     output_dict = {
-        'LOW_ONE': "{}{}".format(str(round(weather['temperatureMin'])), degrees),
-        'HIGH_ONE': "{}{}".format(str(round(weather['temperatureMax'])), degrees),
+        'LOW_ONE': f"{round(weather['temperatureMin'])}{degrees}",
+        'HIGH_ONE': f"{round(weather['temperatureMax'])}{degrees}",
         'ICON_ONE': weather["icon"],
         'WEATHER_DESC_1': weather_desc[1],
         'WEATHER_DESC_2': weather_desc[2],
-        'TIME_NOW_FONT_SIZE': time_now_font_size,
-        'TIME_NOW': time_now,
-        'HOUR_NOW': today.strftime(hour_now_format),
-        'DAY_ONE': today.strftime(day_one_format),
-        'DAY_NAME': today.strftime(day_name_format),
-        'ALERT_MESSAGE_VISIBILITY': "visible" if alert_message else "hidden",
-        'ALERT_MESSAGE': alert_message
+        'TIME_NOW_FONT_SIZE': time_size,
+        'TIME_NOW': time_str,
+        'HOUR_NOW': now.strftime("%H:%M"), # AM/PMを削除
+        'DAY_ONE': now.strftime(date_fmt),
+        'DAY_NAME': now.strftime("%A"),
+        'ALERT_MESSAGE_VISIBILITY': "visible" if alert_msg else "hidden",
+        'ALERT_MESSAGE': alert_msg
     }
 
-    logging.info(output_dict)
-    logging.info("Updating SVG")
-
-    template_svg_filename = f'screen-template.{template_name}.svg'
-    output_svg_filename = 'screen-output-weather.svg'
-    update_svg(template_svg_filename, output_svg_filename, output_dict)
-
+    logging.info(f"Updating SVG using template {template_name}")
+    update_svg(f'screen-template.{template_name}.svg', 'screen-output-weather.svg', output_dict)
 
 if __name__ == "__main__":
     main()
